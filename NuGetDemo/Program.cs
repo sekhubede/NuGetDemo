ab4c12b0d55120da6e1dd9103bf46475338bb9b8
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-06-08 13:50:38 +0200

    Initial commit

---
+++ NuGetDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuGetDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FiggleDemo.Start();
+            PastelDemo.Start();
+
+            WaitForKey();
+        }
+
+        public static void WaitForKey()
+        {
+            Console.Write($"\n(press any key to exit)");
+            Console.ReadKey(true);
+        }
+    }
+
+    internal class FiggleDemo
+    {
+        public static void Start()
+        {
+            Console.WriteLine("=== Figgle Demo ===");
+        }
+    }
+
+    internal class PastelDemo
+    {
+        public static void Start()
+        {
+            Console.WriteLine("=== Pastel Demo ===");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# NuGetDemo
+ Intro referencing NuGet packages in a project.
